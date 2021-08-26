052eadba772b47cb32b9fcf61c788ee75400f077
Ярослав Беркута <yaroslavberkuta@gmail.com>
2021-08-26 16:16:51 +0300

    hero tab

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -575,21 +575,7 @@
 }.call(this));
 
 //main.js file
-
-document.addEventListener("DOMContentLoaded", function () {
-  const tabHover = document.querySelectorAll(".tab__hover");
-  const tab = document.querySelectorAll(".right__tab");
-  for (let i = 0; i < tab.length; i++) {
-    tabHover[i].addEventListener("mouseenter", function () {
-      tab[i].style.flex = "0 0 500px";
-      tab[i].classList.add("remove__after");
-    });
-    tabHover[i].addEventListener("mouseout", function () {
-      tab[i].style.flex = "0 1 200px";
-      tab[i].classList.remove("remove__after");
-    });
-  }
-});
+new WOW().init();
 
 const header = document.querySelector("header");
 let windowScroll = window;
@@ -600,7 +586,6 @@ document.addEventListener("scroll", function () {
     header.classList.remove("header__fixed");
   }
 });
-new WOW().init();
 
 const catalogItem = document.querySelectorAll(".catalog__flex");
 for (let i = 0; i < catalogItem.length; i++) {
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,5 @@
 //main.js file
-
-document.addEventListener("DOMContentLoaded", function () {
-  const tabHover = document.querySelectorAll(".tab__hover");
-  const tab = document.querySelectorAll(".right__tab");
-  for (let i = 0; i < tab.length; i++) {
-    tabHover[i].addEventListener("mouseenter", function () {
-      tab[i].style.flex = "0 0 500px";
-      tab[i].classList.add("remove__after");
-    });
-    tabHover[i].addEventListener("mouseout", function () {
-      tab[i].style.flex = "0 1 200px";
-      tab[i].classList.remove("remove__after");
-    });
-  }
-});
+new WOW().init();
 
 const header = document.querySelector("header");
 let windowScroll = window;
@@ -24,7 +10,6 @@ document.addEventListener("scroll", function () {
     header.classList.remove("header__fixed");
   }
 });
-new WOW().init();
 
 const catalogItem = document.querySelectorAll(".catalog__flex");
 for (let i = 0; i < catalogItem.length; i++) {
