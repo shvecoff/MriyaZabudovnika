20bc1350f400831a4fcc4cf3364a2fa0c7c473b1
Ярослав Беркута <yaroslavberkuta@gmail.com>
2021-08-26 18:24:40 +0300

    react Hero from HomePage

--- mriyazabudovnika/src/index.js
+++ mriyazabudovnika/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.scss";
+import App from "./App.jsx";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById("root")
 );
